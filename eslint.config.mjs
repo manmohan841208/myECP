8e648109d32173a571a0790b9fa9b88e14728dfb
manmohan841208 <manmohan.4.singh@coforge.com>
2025-10-26 00:29:01 +0530

    initial commit for GCP deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,11 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
- 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  {
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-])
- 
-export default eslintConfig
+// import js from "@eslint/js";
+// import globals from "globals";
+// import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
+// import { defineConfig } from "eslint/config";
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+//   tseslint.configs.recommended,
+//   pluginReact.configs.flat.recommended,
+// ]);
